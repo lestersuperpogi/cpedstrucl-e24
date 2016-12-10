bdd0ba0626017e963f5b1993d6ca202e8bc22c36
lestersuperpogi <lester.taguinod23@gmail.com>
2016-12-09 18:50:24 -0800

    Update helloword.cpp

--- helloword.cpp
+++ helloword.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
-int main()
+
+int main ()
 {
-	cout<<"Hello World";
-	cout<<"\ncpedatastructl";
- return 0;
+	cout << "Hello World!" ;
+	cout <<  \n"CPEDSTRUCTL";
+	return 0;
+}
