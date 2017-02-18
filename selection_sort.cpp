40161c6407a8d8ec72f711d80717c7b5ac78077f
Lester Arman Taguinod <lester.taguinod23@gmail.com>
2017-02-18 11:29:07 +0800

    Create selection_sort.cpp

---
+++ selection_sort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int A[ ] = {35,10,55,20,5};
+	int n=5;
+		for(int i=0;i<n-1;i++){
+			int min = A[i], index = 1;
+			for(int j=i+1;j<n;j++){
+				if(A[j]<min){
+					min = A[j];
+					index = j;
+				}
+			}
+			
+				A[index ] = A[i];
+				A[i] = min;
+	for(int k=0;k<n;k++){
+		cout<<A[k];
+		cout<<" ";
+	}
+		cout<<"\n";
+		}
+	return 0;
+}
